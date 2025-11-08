de880bdad7a2673fbffafe83695d9731dec8c1d2
Camila Falaschi <falaschi.camila@yahoo.com.br>
2025-11-07 22:23:40 -0300

    fix: adiciona cors no projeto backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', routes);
 
