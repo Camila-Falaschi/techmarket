e5a32daf4c36f903c32b65b72644c2449ec7250b
Camila Falaschi <falaschi.camila@yahoo.com.br>
2025-11-07 23:13:49 -0300

    fix: ajusta Portas do front e back

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use('/api', routes);
 // health
 app.get('/health', (req, res) => res.json({ ok: true }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
